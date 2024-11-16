239de24698d024d30a3741c18a558b4e7ae5f538
croniccrayon <jamiemiles12@gmail.com>
2024-11-16 10:19:30 +0000

    added word count ability

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+#print("hello world")
+
+def main():
+
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        wordlist = file_contents.split()
+    print(file_contents)
+    print("Word count " , len( wordlist))
+
+main()
