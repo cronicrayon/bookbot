1d2444e38b20a78d631acfe3d7058801cfcf8845
croniccrayon <jamiemiles12@gmail.com>
2024-11-16 11:02:35 +0000

    added character count function

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = get_word_count(text)
+    character_list = character_count(text)
+    print(text)
     print(f"{word_count} words")
+    print(character_list)
+    
 
 def get_word_count(text):
     words = text.split()
@@ -14,7 +18,16 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-#def character_count():
+def character_count(text):
+    uncased_text = text.lower()
+    character_dict = {}
+    for character in uncased_text:
+        if character in character_dict:
+            character_dict[character] += 1
+        else:
+            character_dict[character] = 1
+    return character_dict
+
 
 
 
